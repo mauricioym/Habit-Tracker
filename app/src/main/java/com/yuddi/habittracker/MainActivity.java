b9dfb26888ed78660d3e4a9158768db31fd5418e
Mauricio Miyamura <Mauricio Miyamura>
2016-11-04 17:41:00 -0200

    Move insertNewHabit method to DbHelper class / Insert a method that returns a Cursor instance

--- app/src/main/java/com/yuddi/habittracker/MainActivity.java
+++ app/src/main/java/com/yuddi/habittracker/MainActivity.java
@@ -1,8 +1,6 @@
 package com.yuddi.habittracker;
 
-import android.content.ContentValues;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -24,81 +22,50 @@ public class MainActivity extends AppCompatActivity {
         mDbHelper = new HabitDbHelper(this);
 
         // Insert and log habits examples
-        insertNewHabit("Exercise", 30);
-        insertNewHabit("Wake up at 7:00 am", 21);
-        logDatabaseInfo();
-
-    }
-
-    private void insertNewHabit(String name, int target) {
-
-        SQLiteDatabase db = mDbHelper.getWritableDatabase();
-
-        ContentValues values = new ContentValues();
-        values.put(HabitEntry.COLUMN_HABIT_NAME, name);
-        values.put(HabitEntry.COLUMN_HABIT_STREAK_TARGET, target);
-
-        long newRowId = db.insert(HabitEntry.TABLE_NAME, null, values);
-
-        if (newRowId > 0) {
-            Log.i(LOG_TAG, "Added new habit with id " + newRowId);
-        } else if (newRowId == -1) {
-            Log.e(LOG_TAG, "Error on database insertion");
-        }
-
-    }
-
-    private void logDatabaseInfo() {
-
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
-
-        Cursor cursor = db.query(
-                HabitEntry.TABLE_NAME,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null
-        );
-
-        String result = "";
-
+        mDbHelper.insertNewHabit("Exercise", 30);
+        mDbHelper.insertNewHabit("Wake up at 7:00 am", 21);
+        Cursor cursor = mDbHelper.getDbQuery();
         try {
-
-            result += "Tracking " + cursor.getCount() + " habits.\n\n";
-            result += HabitEntry._ID + " - "
-                    + HabitEntry.COLUMN_HABIT_STREAK + " - "
-                    + HabitEntry.COLUMN_HABIT_MAX_STREAK + " - "
-                    + HabitEntry.COLUMN_HABIT_STREAK_TARGET + "\n";
-
-            int idColumnIndex = cursor.getColumnIndex(HabitEntry._ID);
-            int nameColumnIndex = cursor.getColumnIndex(HabitEntry.COLUMN_HABIT_NAME);
-            int streakColumnIndex = cursor.getColumnIndex(HabitEntry.COLUMN_HABIT_STREAK);
-            int maxStreakColumnIndex = cursor.getColumnIndex(HabitEntry.COLUMN_HABIT_MAX_STREAK);
-            int targetColumnIndex = cursor.getColumnIndex(HabitEntry.COLUMN_HABIT_STREAK_TARGET);
-
-            while (cursor.moveToNext()) {
-                int currentId = cursor.getInt(idColumnIndex);
-                String currentName = cursor.getString(nameColumnIndex);
-                int currentStreak = cursor.getInt(streakColumnIndex);
-                int currentMaxStreak = cursor.getInt(maxStreakColumnIndex);
-                int currentTarget = cursor.getInt(targetColumnIndex);
-
-                result += "\n"
-                        + currentId + " - "
-                        + currentName + " - "
-                        + currentStreak + " - "
-                        + currentMaxStreak + " - "
-                        + currentTarget;
-            }
-
+            logDatabaseQuery(cursor);
         } finally {
             cursor.close();
         }
 
-        Log.i(LOG_TAG, result);
+    }
 
+    private void logDatabaseQuery(Cursor cursor) {
+        String result = "";
+
+        result += "Tracking " + cursor.getCount() + " habits.\n\n";
+        result += HabitEntry._ID + " - "
+                + HabitEntry.COLUMN_HABIT_NAME + " - "
+                + HabitEntry.COLUMN_HABIT_STREAK + " - "
+                + HabitEntry.COLUMN_HABIT_MAX_STREAK + " - "
+                + HabitEntry.COLUMN_HABIT_STREAK_TARGET + "\n";
+
+        int idColumnIndex = cursor.getColumnIndex(HabitEntry._ID);
+        int nameColumnIndex = cursor.getColumnIndex(HabitEntry.COLUMN_HABIT_NAME);
+        int streakColumnIndex = cursor.getColumnIndex(HabitEntry.COLUMN_HABIT_STREAK);
+        int maxStreakColumnIndex = cursor.getColumnIndex(HabitEntry.COLUMN_HABIT_MAX_STREAK);
+        int targetColumnIndex = cursor.getColumnIndex(HabitEntry.COLUMN_HABIT_STREAK_TARGET);
+
+        while (cursor.moveToNext()) {
+            int currentId = cursor.getInt(idColumnIndex);
+            String currentName = cursor.getString(nameColumnIndex);
+            int currentStreak = cursor.getInt(streakColumnIndex);
+            int currentMaxStreak = cursor.getInt(maxStreakColumnIndex);
+            int currentTarget = cursor.getInt(targetColumnIndex);
+
+            result += "\n"
+                    + currentId + " - "
+                    + currentName + " - "
+                    + currentStreak + " - "
+                    + currentMaxStreak + " - "
+                    + currentTarget;
+        }
+
+
+        Log.i(LOG_TAG, result);
     }
 
 }
--- app/src/main/java/com/yuddi/habittracker/data/HabitDbHelper.java
+++ app/src/main/java/com/yuddi/habittracker/data/HabitDbHelper.java
@@ -1,8 +1,11 @@
 package com.yuddi.habittracker.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import com.yuddi.habittracker.data.HabitContract.HabitEntry;
 
@@ -11,7 +14,9 @@ import com.yuddi.habittracker.data.HabitContract.HabitEntry;
  */
 
 public class HabitDbHelper extends SQLiteOpenHelper {
-    
+
+    private static final String LOG_TAG = HabitDbHelper.class.getName();
+
     private static final String DATABASE_NAME = "habit_tracker.db";
     
     private static final int DATABASE_VERSION = 1;
@@ -34,7 +39,50 @@ public class HabitDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        
+        // The database is still at version 1, so the database won't be upgraded.
     }
-    
+
+    public void insertNewHabit(String name, int target) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(HabitEntry.COLUMN_HABIT_NAME, name);
+        values.put(HabitEntry.COLUMN_HABIT_STREAK_TARGET, target);
+
+        long newRowId = db.insert(HabitEntry.TABLE_NAME, null, values);
+        db.close();
+
+        if (newRowId == -1) {
+            Log.e(LOG_TAG, "Error on database insertion");
+        } else {
+            Log.i(LOG_TAG, "Added new habit with id " + newRowId);
+        }
+
+    }
+
+    public Cursor getDbQuery() {
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String[] projection = new String[]{
+                HabitEntry._ID,
+                HabitEntry.COLUMN_HABIT_NAME,
+                HabitEntry.COLUMN_HABIT_STREAK,
+                HabitEntry.COLUMN_HABIT_MAX_STREAK,
+                HabitEntry.COLUMN_HABIT_STREAK_TARGET
+        };
+
+        return db.query(
+                HabitEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+    }
+
 }
