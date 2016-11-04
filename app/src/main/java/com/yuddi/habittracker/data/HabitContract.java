65e889fdfd08d5938298b23c8aaccedca6291b32
Mauricio Miyamura <Mauricio Miyamura>
2016-11-04 12:03:55 -0200

    Insert HabitContract and HabitDbHelper classes

---
+++ app/src/main/java/com/yuddi/habittracker/data/HabitContract.java
@@ -0,0 +1,25 @@
+package com.yuddi.habittracker.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Mauricio on 11/4/2016.
+ */
+
+public final class HabitContract {
+
+    private HabitContract() {
+        throw new AssertionError("No HabitContract instances for you!");
+    }
+
+    public static abstract class HabitEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "habits";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_HABIT_NAME = "name";
+        public static final String COLUMN_HABIT_STREAK = "streak";
+        public static final String COLUMN_HABIT_MAX_STREAK = "max_streak";
+
+    }
+}
---
+++ app/src/main/java/com/yuddi/habittracker/data/HabitDbHelper.java
@@ -0,0 +1,39 @@
+package com.yuddi.habittracker.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.yuddi.habittracker.data.HabitContract.HabitEntry;
+
+/**
+ * Created by Mauricio on 11/4/2016.
+ */
+
+public class HabitDbHelper extends SQLiteOpenHelper {
+    
+    private static final String DATABASE_NAME = "habit_tracker.db";
+    
+    private static final int DATABASE_VERSION = 1;
+    
+    public HabitDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_PETS_TABLE = "CREATE TABLE " + HabitEntry.TABLE_NAME + "("
+                + HabitEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + HabitEntry.COLUMN_HABIT_NAME + " TEXT NOT NULL, "
+                + HabitEntry.COLUMN_HABIT_STREAK + " INTEGER NOT NULL DEFAULT 0, "
+                + HabitEntry.COLUMN_HABIT_MAX_STREAK + " INTEGER NOT NULL DEFAULT 0);";
+
+        db.execSQL(SQL_CREATE_PETS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        
+    }
+    
+}
